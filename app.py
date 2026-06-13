2f347449373d75b34922476988751b07dcf9542f
jgoud00 <vinnu052007@gmail.com>
2026-06-13 18:48:27 +0530

    Fix absolute paths for static and templates to resolve Vercel server error

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -9,9 +10,11 @@ import math
 
 app = FastAPI()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # Mount static and templates
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 class Edge(BaseModel):
     node: str
